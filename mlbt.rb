f6fdee2be969100f1ac03ddb6099dd4ba03a197c
Andrew Schneider <andjschneider@gmail.com>
2021-07-13 14:42:35 -0700

    v0.0.9

--- mlbt.rb
+++ mlbt.rb
@@ -1,10 +1,10 @@
 class Mlbt < Formula
   desc "Interact with MLB's Statcast API and even watch a live game ⚾"
   homepage "https://github.com/mlb-rs/mlbt"
-  version "v0.0.8"
+  version "v0.0.9"
   license "MIT"
   url "https://github.com/mlb-rs/mlbt/releases/download/#{version}/mlbt-macos.tar.gz"
-  sha256 "3203f3cf174b417cc22f2dc01787dfa232052b7e0c3bb1b2435b5575b9aa6ee3"
+  sha256 "5fcb724042d268ca240ae3329f4aa3d3660cc35c891f06f1574c11d6ff4e637e"
 
   def install
     bin.install "mlbt"
